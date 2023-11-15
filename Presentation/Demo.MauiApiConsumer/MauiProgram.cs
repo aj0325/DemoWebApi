a186c2b219a5e090808beeb850e7595e9d6b698c
Coding Droplets <codingdroplets@gmail.com>
2023-11-16 01:30:44 +0530

    231116

---
+++ Presentation/Demo.MauiApiConsumer/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Demo.ApiClient.IoC;
+
+namespace Demo.MauiApiConsumer
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddDemoApiClientService(x => x.ApiBaseAddress = "http://10.0.2.2:5015/");
+            builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
